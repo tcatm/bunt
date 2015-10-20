76a42502658155277e07392b5d95b2c3342089ea
Nils Schneider <nils@nilsschneider.net>
2015-10-20 03:42:07 +0200

    more work

--- fft.py
+++ fft.py
@@ -2,14 +2,20 @@ import numpy as np
 import socket
 import math
 import colorsys
+import pyaudio
+import struct
 from array import array
 from time import sleep, clock
 from pylut import *
+from scipy.fftpack import rfft
 
 UDP_IP = "2a01:170:1112:0:bad8:12ff:fe66:fa1"
 UDP_PORT = 2812
 
 sock = socket.socket(socket.AF_INET6,socket.SOCK_DGRAM)
+
+def hsv(h, s, v):
+    return np.array(colorsys.hsv_to_rgb(h, s, v)) * 255
 
 def setleds(lut, data):
     """
@@ -21,21 +27,17 @@ def setleds(lut, data):
     data = [item for sublist in data for item in sublist]
     sock.sendto(array('B', data).tostring(), (UDP_IP, UDP_PORT))
 
-import pyaudio
-import struct
-import wave
-
 FPS = 30
 
-nFFT = 2048
+nFFT = 1024
 BUF_SIZE = 1 * nFFT
 FORMAT = pyaudio.paInt16
 CHANNELS = 2
-RATE = 96000
+RATE = 48000
+FSCALE = 1 / (12000 / (RATE / 2.0))
 
-FALL = 0.71
-RISE = 0.95
-OVERSHOOT = 0.3
+FALL = 0.87
+RISE = 0.9
 
 state = dict(l=np.zeros(119), r=np.zeros(119))
 
@@ -65,7 +67,7 @@ def bezier_gradient(colors):
       points. Dictionary also contains control
       colors/points. '''
   # RGB vectors for each color, use as control points
-  RGB_list = colors
+  RGB_list = list([hsv(x[0], x[1], x[2]) for x in colors])
   n = len(RGB_list) - 1
 
   def bezier_interp(t):
@@ -73,7 +75,7 @@ def bezier_gradient(colors):
         for this specific curve'''
     # List of all summands
     summands = [
-      map(lambda x: int(bernstein(t,n,i)*x), c)
+      list(map(lambda x: int(bernstein(t,n,i)*x), c))
       for i, c in enumerate(RGB_list)
     ]
     # Output color
@@ -85,54 +87,59 @@ def bezier_gradient(colors):
 
     return out
 
-  return bezier_interp
+  steps = 64
 
-grad = bezier_gradient([[0,0,0], [10,0,10], [255,0,0], [255,150,150], [255,100,0], [255, 255, 0], [255, 130, 0], [255, 255, 200]])
-#grad = bezier_gradient([[0,0,0], [0,0,0], [80,0,80], [80,0,80], [0,255,0], [0,40,0], [0, 50, 0], [0, 80, 0]] * 2)
-#grad = bezier_gradient([[0,0,0], [255, 0, 0], [255, 255, 0], [0, 255, 0], [0, 255, 255], [0, 0, 255], [255, 255, 255]])
-#grad = bezier_gradient([[0, 0, 0], [255, 255, 255]])
+  xp = range(0, steps+1)
+  y = list(bezier_interp(x/float(steps)) for x in xp)
+  xp = list(x/float(steps) for x in xp)
+  yr = list(x[0] for x in y)
+  yg = list(x[1] for x in y)
+  yb = list(x[2] for x in y)
+
+  def f(x):
+      return (np.interp(x, xp, yr), np.interp(x, xp, yg), np.interp(x, xp, yb))
+
+  return f
+
+
+# Feuer
+grad = bezier_gradient([[0, 0, 0], [0.6, 1, 0.10], [0.05, 1, 1], [0.1, 1, 1], [0, 1, 1], [0.17, 0.6, 1], [0.15, 0.1, 1]])
+# Gruen
+#grad = bezier_gradient([[0, 0, 0], [0.66, 0.5, 0.3], [0.5, 1, 1], [0.13, 1, 1], [0.3, 1, 1]])
+#grad = bezier_gradient([[0, 0, 0], [0.9, 0.0, 1]])
 
 def animate(lut, stream, MAX_y, state):
   # Read n*nFFT frames from stream, n > 0
-  N = max(stream.get_read_available() / nFFT, 1) * nFFT
+  N = stream.get_read_available()
   data = stream.read(N)
 
   # Unpack data, LRLRLR...
-  y = np.array(struct.unpack("%dh" % (N * CHANNELS), data)) / MAX_y
-  y_L = y[::2]
-  y_R = y[1::2]
+  y = np.frombuffer(data, dtype=np.dtype("int16"))
 
-  Y_L = np.fft.fft(y_L, nFFT)
-  Y_R = np.fft.fft(y_R, nFFT)
+  N = 0.1 / MAX_y
 
-#  print(list(np.fft.fftfreq(nFFT, 1.0/RATE)[0:nFFT/2]))
-
-  Y_L = Y_L[0:nFFT/2]
-  Y_R = Y_R[0:nFFT/2]
-
-  N = 0.1
-
-  Y_L = N * abs(Y_L)
-  Y_R = N * abs(Y_R)
+  Y_L = N * rfft(y[::2], nFFT)
+  Y_R = N * rfft(y[1::2], nFFT)
 
   l_l = np.zeros(119)
   l_r = np.zeros(119)
 
   for i in range(0, 119):
-    x = int((i*i)/(math.pow(119,2))* nFFT/16)
-    y = int(((i+1)*(i+1))/(math.pow(119,2))* nFFT/16) + 1
+    x = int((i*i)/(math.pow(119,2))* nFFT / FSCALE)
+    y = int(((i+1)*(i+1))/(math.pow(119,2))* nFFT / FSCALE) + 1
     l_l[i] = sum(Y_L[x:y])
     l_r[i] = sum(Y_R[x:y])
 
   l_l = np.minimum(1, l_l)
-  l_l = (1 - RISE) * state['l'] + RISE * np.maximum(FALL * state['l'], l_l) + OVERSHOOT * l_l
+  l_l = (1 - RISE) * state['l'] + RISE * np.maximum(FALL * state['l'], l_l)
   state['l'] = l_l
 
   l_r = np.minimum(1, l_r)
-  l_r = (1 - RISE) * state['r'] + RISE * np.maximum(FALL * state['r'], l_r) + OVERSHOOT * l_r
+  l_r = (1 - RISE) * state['r'] + RISE * np.maximum(FALL * state['r'], l_r)
   state['r'] = l_r
 
   data = [grad(min(1, x)) for x in 1 * np.hstack((np.flipud(l_r), l_l))]
+#  data = [[x * 255, x * 255, 0] for x in 1 * np.hstack((np.flipud(l_r), l_l))]
 
   setleds(lut, data)
 
@@ -161,6 +168,7 @@ def main():
       t1 = clock()
       dt = t1 - t0
       delay = max(0, 1.0/FPS - dt)
+      print(1/(delay + dt))
       sleep(delay)
   except KeyboardInterrupt:
     print("stop")
